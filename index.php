6ff89013e998205e315d7c6e9bf3833dd8245bb2
Bola Naguib <bolanaguib@gmail.com>
2019-03-13 00:30:54 +0200

    added more items

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 
   <!-- UIkit CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 </head>
 
@@ -22,13 +23,15 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
 
       <!-- UIkit CSS -->
+      <link rel="stylesheet" href="style.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
+      <link rel="stylesheet" href="style.css">
 
     </head>
 
     <body>
       <div class="header">
-        <div class="uk-container">
+        <div class="uk-container uk-container-large">
           <nav class="" uk-navbar>
             <div class="uk-navbar-left">
               <a href="#"><img src="img/logo.png" width="200" alt=""></a>
@@ -36,8 +39,8 @@
             <div class="uk-navbar-right">
 
               <ul class="uk-navbar-nav">
-                <li class="uk-active"><a href="#">Active</a></li>
-                <li>
+                <li class="uk-active"><a href="#">Home</a></li>
+                <!-- <li>
                   <a href="#">Parent</a>
                   <div class="uk-navbar-dropdown">
                     <ul class="uk-nav uk-navbar-dropdown-nav">
@@ -46,8 +49,12 @@
                       <li><a href="#">Item</a></li>
                     </ul>
                   </div>
-                </li>
-                <li><a href="#">Item</a></li>
+                </li> -->
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a class="quote" href="#">Request a quote </a></li>
+
               </ul>
 
             </div>
@@ -58,26 +65,26 @@
       </div>
 
 <section>
-  <div class="uk-container">
+  <div class="uk-container uk-container-large">
     <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: fade">
 
         <ul class="uk-slideshow-items">
             <li>
-                <img src="img/slider.jpg" alt="" uk-cover>
+                <img src="img/slider.jpeg" alt="" uk-cover>
                 <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
                     <h3 class="uk-margin-remove">Bottom</h3>
                     <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
             </li>
             <li>
-                <img src="img/slider.jpg alt="" uk-cover>
+                <img src="img/slider2.jpeg" alt="" uk-cover>
                 <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
                     <h3 class="uk-margin-remove">Bottom</h3>
                     <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
             </li>
             <li>
-                <img src="img/slider.jpg" alt="" uk-cover>
+                <img src="img/slider.jpeg" alt="" uk-cover>
                 <div class="uk-overlay uk-overlay-primary uk-position-right uk-text-center uk-transition-slide-right uk-width-medium">
                     <h3 class="uk-margin-remove">Left</h3>
                     <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -91,8 +98,93 @@
     </div>
   </div>
 </section>
+<section class="cta uk-padding">
+  <div class="uk-container uk-container-large">
+    <div uk-grid>
+      <div class="uk-width-1-2">
+<h4 class="cta_text"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h4>
+      </div>
+      <div class="uk-width-1-2 uk-text-right">
+<a class="cta_button" href="#"> Click Me </a>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="uk-section">
+  <div class="uk-container uk-container-large">
+    <div uk-grid>
+      <div class="uk-width-1-2">
+<h1 class="big_title"> HERE WE WILL HAVE A BIG TITLE </h1>
+<h3 class="uk-margin-large-bottom"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </h3>
+<a class="big_button" href="#"> Click HEre </a>
+      </div>
+      <div class="uk-width-1-2">
+            <div class="uk-card-header">
+                <div class="uk-grid-small uk-flex-middle" uk-grid>
+                    <div class="uk-width-auto">
+                      <div class="icons">
+                        <i class="fas fa-link"></i>
 
+                      </div>
+                    </div>
+                    <div class="uk-width-expand">
+                        <h3 class="uk-card-title icons__title uk-margin-remove-bottom">Lorem Ipsum </h3>
+                        <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</time></p>
+                    </div>
+                </div>
 
+        </div>
+
+        <div class="uk-card-header">
+            <div class="uk-grid-small uk-flex-middle" uk-grid>
+                <div class="uk-width-auto">
+                  <div class="icons">
+                    <i class="fas fa-link"></i>
+
+                  </div>
+                </div>
+                <div class="uk-width-expand">
+                    <h3 class="uk-card-title icons__title uk-margin-remove-bottom">Lorem Ipsum </h3>
+                    <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</time></p>
+                </div>
+            </div>
+
+    </div>
+
+    <div class="uk-card-header">
+        <div class="uk-grid-small uk-flex-middle" uk-grid>
+            <div class="uk-width-auto">
+              <div class="icons">
+                <i class="fas fa-link"></i>
+
+              </div>
+            </div>
+            <div class="uk-width-expand">
+                <h3 class="uk-card-title icons__title uk-margin-remove-bottom">Lorem Ipsum </h3>
+                <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</time></p>
+            </div>
+        </div>
+
+</div>
+      </div>
+    </div>
+    <hr>
+  </div>
+</section>
+<section class="uk-section">
+<div class="uk-container uk-container-large">
+  <div class="uk-flex uk-flex-middle" uk-grid>
+    <div class="uk-width-1-2">
+      <img src="img/slider.jpeg"  alt="">
+    </div>
+    <div class="uk-width-1-2">
+      <div class="image__over">
+        <h1>  Lorem Ipsum is simply dummy text of the printing </h1>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
 
 
 
